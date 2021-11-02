cfbb08a8fc46cebc831d8ce36a475e99fed3437a
Eduard Philip <eduardophilip95@gmail.com>
2021-11-02 02:38:20 +0000

    fix spelling

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -28,17 +28,17 @@ export class HttpService {
   getGameDetais(id: string): Observable<Game> {
     const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`);
     const gameTrailersRequest = this.http.get(`${env.BASE_URL}/games/${id}/movies`);
-    const gameScreenshortsRequest = this.http.get(`${env.BASE_URL}/games/${id}/screenshots`);
+    const gameScreenshotsRequest = this.http.get(`${env.BASE_URL}/games/${id}/screenshots`);
 
     return forkJoin({
       gameInfoRequest,
       gameTrailersRequest,
-      gameScreenshortsRequest,
+      gameScreenshotsRequest,
     }).pipe(
       map((resp: any) => {
         return {
           ...resp['gameInfoRequest'],
-          screemshorts: resp['gameScreenshortsRequest']?.results,
+          screenshots: resp['gameScreenshortsRequest']?.results,
           trailers: resp['gameTrailersRequest']?.results,
         }
       })
